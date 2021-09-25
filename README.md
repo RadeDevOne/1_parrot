0056bcd46065f9b29014e0ae04bb8c8a3187e2c5
Rade <rade.bajic.dev@gmail.com>
2021-09-25 20:37:29 +0200

    🦉

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ## STYLING
 
-twin.macro:
+twin.macro with emotion (explained)
 
 <https://github.com/ben-rogerson/twin.examples/tree/master/next-emotion>
+
+
+typescript example (very nice, has more stuff):
+
+<https://github.com/ben-rogerson/twin.examples/tree/master/next-emotion-typescript>
+
--- components/1_index_page/Test.tsx
+++ components/1_index_page/Test.tsx
@@ -1,6 +1,6 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import type { FC } from "react";
-import tw, { css, styled } from "twin.macro";
+import tw, { css, styled, theme } from "twin.macro";
 
 const StyledTwDiv = tw.div`
 
@@ -8,14 +8,18 @@ const StyledTwDiv = tw.div`
 
 `;
 
+const StyledDiv = styled.div`
+  background-color: blanchedalmond;
+`;
+
 const TestComponent: FC = () => {
   return (
-    <section css={[tw`bg-gray-200`, tw`hover:bg-green-400`]}>
+    <section css={[tw`bg-gray-200`, tw`hover:text-blue-600`]}>
       <div
         css={css`
           border: crimson solid 1px;
           & h1 {
-            font-size: 2em;
+            font-size: 4em;
           }
           & p {
             color: #421f35;
@@ -25,7 +29,32 @@ const TestComponent: FC = () => {
         <h1>Test</h1>
         <p>Lorem ipsum</p>
         <StyledTwDiv>Hello World</StyledTwDiv>
+        <StyledDiv>Hello world</StyledDiv>
       </div>
+      <aside
+        css={css`
+          font-size: 2rem;
+          & h1 {
+            text-decoration-line: underline;
+          }
+
+          /*  YOU CAN PARSE TAILWIND CLASSES TO CSS */
+
+          ${tw`border-green-500 border-4 `}
+        `}
+      >
+        <h1>Lorem Blorem</h1>
+      </aside>
+      <footer
+        css={css`
+          font-size: 4rem;
+          /* YOU CAN ALSO REFERENCE STYLES FROM YOUR THEME */
+
+          background-color: ${theme`colors.electric`};
+        `}
+      >
+        <h2>Hello World</h2>
+      </footer>
     </section>
   );
 };
