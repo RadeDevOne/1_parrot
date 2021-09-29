a23afc55e3bc7ecd5dc5fa87478dfd05fa00c2fd
Rade <rade.bajic.dev@gmail.com>
2021-09-29 18:24:41 +0200

    🦉

--- components/cart/ShoppingCart.tsx
+++ components/cart/ShoppingCart.tsx
@@ -13,13 +13,15 @@ import CartContent from "./CartContent";
 const ShoppingCart: FC = () => {
   const [cartUIState, cartUIDispatch] = useActor(headerNCartService);
 
+  const cartIsVisible = cartUIState.value === fse.cart_visible;
+
   return (
     <>
       <motion.section
         animate={{
-          translateX: cartUIState.value === fse.cart_visible ? "0%" : "80%",
-          translateY: cartUIState.value === fse.cart_visible ? "0%" : "-100%",
-          scale: cartUIState.value === fse.cart_visible ? 1 : 0,
+          translateX: cartIsVisible ? "0%" : "80%",
+          translateY: cartIsVisible ? "0%" : "-100%",
+          scale: cartIsVisible ? 1 : 0,
         }}
         transition={{
           duration: 0.4,
@@ -32,31 +34,42 @@ const ShoppingCart: FC = () => {
         className="cart-wide"
         tw="light:bg-l dark:bg-gray-800 transform-gpu xl:flex lg:flex md:flex sm:hidden hidden"
         css={css`
-          /* border: crimson solid 2px; */
-          position: absolute;
+          border: crimson solid 2px;
+          position: fixed;
           top: 0;
-          height: 60vh;
+          /* height: 60vh; */
           width: 100vw;
+          height: 100vh;
           /* transform: translateX(80%) translateY(5%); */
         `}
       >
         <CartContent />
         {/*  */}
       </motion.section>
-      <section
+      <motion.section
+        animate={{
+          top: cartIsVisible ? 0 : "auto",
+          height: cartIsVisible ? "100vh" : "0vh",
+        }}
+        transition={{
+          duration: 0.2,
+        }}
+        initial={{
+          top: "auto",
+          height: "0vh",
+        }}
         className="cart-mobile"
-        tw="transform-gpu flex md:hidden"
+        tw="dark:bg-d light:bg-l transform-gpu flex md:hidden"
         css={css`
-          border: crimson solid 2px;
-          position: absolute;
-          top: 46px;
-          height: 60vh;
+          /* border: crimson solid 2px; */
+          position: fixed;
+
           width: 100vw;
-          /* transform: translateX(-98%); */
+          overflow: hidden;
         `}
       >
-        <div></div>
-      </section>
+        <CartContent />
+      </motion.section>
     </>
   );
 };
