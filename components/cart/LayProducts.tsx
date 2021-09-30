7b66f3db2533f569e7b054743dcf0ce5a5c875d1
Rade <rade.bajic.dev@gmail.com>
2021-09-30 02:01:50 +0200

    🦉

--- components/cart/LayProducts.tsx
+++ components/cart/LayProducts.tsx
@@ -122,7 +122,7 @@ const LayProducts: FC = () => {
             <div tw="flex w-full space-x-2 sm:space-x-4">
               <img
                 tw="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500"
-                src="https://images.unsplash.com/phodark:to-1594549181132-9045fed330ce?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=675&amp;q=80"
+                src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80"
                 alt="Set of travel chargers"
               />
               <div tw="flex flex-col justify-between w-full pb-4">
