a0dba84def3e0f5b96e5b6a16152e7cdc8359ec3
Rade <rade.bajic.dev@gmail.com>
2021-10-02 21:23:24 +0200

    🦉

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -18,6 +18,8 @@ import { signIn, useSession } from "next-auth/client";
 //
 //
 
+import Spinner from "@/components/common/Spinner";
+
 const SignInPage: NP = () => {
   const { push, asPath } = useRouter();
   const [session, loading] = useSession();
@@ -111,9 +113,7 @@ const SignInPage: NP = () => {
                   margin-left: 8px;
                 `}
               >
-                <div tw="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
-                  <div tw="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-                </div>
+                <Spinner />
               </div>
             ) : (
               ""
