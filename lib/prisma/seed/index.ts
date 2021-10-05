689d51fef1e96fbe4e67f11a7be405306eb98f37
RadeDev <bajic.rade2@gmail.com>
2021-10-05 22:53:13 +0200

    🦉

--- lib/prisma/seed/index.ts
+++ lib/prisma/seed/index.ts
@@ -11,8 +11,13 @@ const unsplashTemplate = (name: string) => {
 
 export const generateProfilesData = (numberOfProfiles: number) => {
   const profilesData: Profile[] = [];
+  const profileIds: string[] = [];
 
   for (let i = 0; i < numberOfProfiles; i++) {
+    const id = cuid();
+
+    profileIds.push(id);
+
     profilesData.push({
       city: faker.address.city(),
       country: faker.address.country(),
@@ -29,11 +34,12 @@ export const generateProfilesData = (numberOfProfiles: number) => {
     });
   }
 
-  return profilesData;
+  return { profilesData, profileIds };
 };
 
 export const generateProductData = (numberOfProducts: number) => {
   const productsData: Product[] = [];
+  const productIds: string[] = [];
 
   for (let i = 0; i < numberOfProducts; i++) {
     // WE WILL DO THAT EVERY OTHER PRODUCT IS OUT OF STOCK
@@ -59,5 +65,52 @@ export const generateProductData = (numberOfProducts: number) => {
     });
   }
 
-  return productsData;
+  return { productsData, productIds };
+};
+
+// FOR Reviews WE WOULD NEED EXISTING PRODUCTS
+// AND EXISTING PROFILES
+
+export const generateReviewsData = (
+  prdoductIds: string[],
+  profileIds: string[]
+) => {
+  const reviewsData: Review[] = [];
+
+  // LETS SAY EVERY PRODUCT SHOUD HAVE 4 REVIEEWS
+
+  for (let i = 0; i < prdoductIds.length; i++) {
+    let profileNo = 0;
+    let back = false;
+
+    const profileId = profileIds[profileNo];
+
+    for (let j = 0; j < 5; j++) {
+      //
+      //
+      reviewsData.push({
+        id: cuid(),
+        productId: prdoductIds[i],
+        profileId,
+        comment: faker.lorem.sentences(),
+        createdAt: faker.date.past(),
+        rating: Math.round(Math.random() * 5.1) || 1,
+        updatedAt: faker.date.future(),
+      });
+
+      if (back) {
+        profileNo--;
+      } else {
+        profileNo++;
+      }
+
+      if (profileNo === profileIds.length) {
+        back = true;
+      } else if (profileNo < 0) {
+        back = false;
+      }
+    }
+  }
+
+  return reviewsData;
 };
