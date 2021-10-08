42260343bb6135f4dc11b7dd87e537ff12cfd758
RadeDev <rade.bajic.dev@gmail.com>
2021-10-08 18:03:31 +0200

    🦉

--- pages/api/EXAMPLE/[foo]/baz.ts
+++ pages/api/EXAMPLE/[foo]/baz.ts
@@ -6,8 +6,8 @@ const handler = nc<NextApiRequest, NextApiResponse>();
 handler.post(async (req, res) => {
   const { foo } = req.query;
 
-  console.log({ BODY: req.body });
-  console.log({ HEADERS: req.headers });
+  // console.log({ BODY: req.body });
+  // console.log({ HEADERS: req.headers });
 
   return res.status(200).json({ baz: `hello 666 ${foo}` });
 });
