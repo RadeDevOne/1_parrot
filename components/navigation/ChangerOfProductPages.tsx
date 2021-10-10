8da7f9dd7562cb5714ac9c9121c69168d987dba9
RadeDev <rade.bajic.dev@gmail.com>
2021-10-10 15:29:57 +0200

    🦉

--- components/navigation/ChangerOfProductPages.tsx
+++ components/navigation/ChangerOfProductPages.tsx
@@ -1,19 +1,58 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import type { FC } from "react";
 import tw, { css, styled, theme } from "twin.macro";
+import Link from "next/link";
 
-import calculatePaginationNumbers from "@/util/calculatePaginationNumbers";
+import pagCalc from "@/util/pagCalc";
 
-const ChangerOfProductsPages: FC = () => {
+export interface PropsI {
+  currentPageNumber: number;
+  totalItems: number;
+}
+
+const ChangerOfProductsPages: FC<PropsI> = ({
+  currentPageNumber,
+  totalItems,
+}) => {
+  const basePath = "/products";
+
+  const paginationData = pagCalc(currentPageNumber, totalItems);
+
   return (
-    <section css={[tw`bg-gray-200`, tw`hover:text-indigo-600`]}>
-      <div
-        css={css`
-          background-color: ${theme`colors.electric`};
-        `}
-      ></div>
-      {/*  */}
-    </section>
+    <div tw="flex justify-center">
+      <div tw="flex rounded-md mt-8">
+        <a
+          href="#"
+          tw="py-2 px-4 leading-tight bg-white border border-gray-200 text-blue-700 border-r-0 ml-0 rounded-l hover:bg-blue-500 hover:text-white"
+        >
+          <span>Previous</span>
+        </a>
+        <a
+          href="#"
+          tw="py-2 px-4 leading-tight bg-white border border-gray-200 text-blue-700 border-r-0 hover:bg-blue-500 hover:text-white"
+        >
+          <span>1</span>
+        </a>
+        <a
+          href="#"
+          tw="py-2 px-4 leading-tight bg-white border border-gray-200 text-blue-700 border-r-0 hover:bg-blue-500 hover:text-white"
+        >
+          <span>2</span>
+        </a>
+        <a
+          href="#"
+          tw="py-2 px-4 leading-tight bg-white border border-gray-200 text-blue-700 border-r-0 hover:bg-blue-500 hover:text-white"
+        >
+          <span>3</span>
+        </a>
+        <a
+          href="#"
+          tw="py-2 px-4 leading-tight bg-white border border-gray-200 text-blue-700 rounded-r hover:bg-blue-500 hover:text-white"
+        >
+          <span>Next</span>
+        </a>
+      </div>
+    </div>
   );
 };
 
