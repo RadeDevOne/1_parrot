9f859229bda2807e49853cb9e3ae9ba4e5414007
RadeDev <rade.bajic.dev@gmail.com>
2021-10-10 02:26:43 +0200

    🦉

---
+++ components/navigation/ChangerOfProductPages.tsx
@@ -0,0 +1,20 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import type { FC } from "react";
+import tw, { css, styled, theme } from "twin.macro";
+
+import calculatePaginationNumbers from "@/util/calculatePaginationNumbers";
+
+const ChangerOfProductsPages: FC = () => {
+  return (
+    <section css={[tw`bg-gray-200`, tw`hover:text-indigo-600`]}>
+      <div
+        css={css`
+          background-color: ${theme`colors.electric`};
+        `}
+      ></div>
+      {/*  */}
+    </section>
+  );
+};
+
+export default ChangerOfProductsPages;
