e50fc3bdb5d914409e12737560f5ae8034cc337c
RadeDev <rade.bajic.dev@gmail.com>
2021-10-10 15:38:29 +0200

    🦉

--- components/products/Products.tsx
+++ components/products/Products.tsx
@@ -1,33 +1,45 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import type { FC } from "react";
+import { Fragment } from "react";
 import tw /* , { css, styled, theme } */ from "twin.macro";
 
 import Product from "./Product";
+
+import ChangerOfProductsPages from "../navigation/ChangerOfProductPages";
 
 import type { PropsI } from "@/pages/index";
 
 export interface ProductsPropsI {
   products: PropsI["products"];
+  totalProducts: PropsI["totalProducts"];
 }
 
-const Products: FC<ProductsPropsI> = ({ products }) => {
+const Products: FC<ProductsPropsI> = ({ products, totalProducts }) => {
   return (
-    <section>
-      <div tw="container mx-auto px-6">
-        <h3 tw="light:text-gray-700 dark:text-gray-200 text-2xl font-medium">
-          We have everything
-        </h3>
-        <span tw="mt-3 text-sm light:text-gray-500 dark:text-gray-600">
-          200+ Products
-        </span>
+    <Fragment>
+      <section>
+        <div tw="container mx-auto px-6">
+          <h3 tw="light:text-gray-700 dark:text-gray-200 text-2xl font-medium">
+            We have everything
+          </h3>
+          <span tw="mt-3 text-sm light:text-gray-500 dark:text-gray-600">
+            200+ Products
+          </span>
 
-        <div tw="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-          {products.map((product, i) => {
-            return <Product key={`${i}-${product.id}`} product={product} />;
-          })}
+          <div tw="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+            {products.map((product, i) => {
+              return <Product key={`${i}-${product.id}`} product={product} />;
+            })}
+          </div>
         </div>
+      </section>
+      <div tw="mb-10">
+        <ChangerOfProductsPages
+          currentPageNumber={0}
+          totalItems={totalProducts}
+        />
       </div>
-    </section>
+    </Fragment>
   );
 };
 
