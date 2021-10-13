a8e825775471f201dc5997628d9a14a4bcf8fc89
RadeDev <rade.bajic.dev@gmail.com>
2021-10-13 19:45:23 +0200

    🦉

--- components/reviews/Reviews.tsx
+++ components/reviews/Reviews.tsx
@@ -11,6 +11,8 @@ interface PropsI {
 }
 
 const Reviews: FC<PropsI> = ({ reviews }) => {
+  const unsplash_url = "https://source.unsplash.com/100x100/?face";
+
   return (
     <section tw="mt-8 mb-8">
       <div tw=" dark:inherits[inherit] antialiased mx-auto max-w-screen-sm px-1 ">
@@ -23,14 +25,17 @@ const Reviews: FC<PropsI> = ({ reviews }) => {
             <div tw="flex-shrink-0 mr-3">
               <img
                 tw="mt-2 rounded-full w-8 h-8 sm:w-10 sm:h-10"
-                src="https://images.unsplash.com/photo-1604426633861-11b2faead63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=80"
+                // src="https://images.unsplash.com/photo-1604426633861-11b2faead63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=80"
+                src={unsplash_url}
                 alt="profile"
               />
             </div>
             <div tw="flex-1 rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed">
               <strong tw="dark:text-gray-300">Sarah</strong>{" "}
               <span tw="text-xs text-gray-400">3:34 PM</span>
-              <span tw="ml-2 text-green-600 text-xs">Verified Buyer</span>
+              <span tw="inline-block ml-2 text-green-600 text-xs">
+                Verified Buyer
+              </span>
               <div tw="flex items-center mt-1 mb-0.5">
                 <svg
                   tw="w-4 h-4 fill-current text-yellow-600"
