afe4d02a038b113878bf7448aebf5ab2e35f7d0b
RadeDev <rade.bajic.dev@gmail.com>
2021-10-13 18:22:37 +0200

    🦉

--- components/products/OutOfStockInfo.tsx
+++ components/products/OutOfStockInfo.tsx
@@ -1,9 +1,11 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import type { FC } from "react";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import tw, { css, styled, theme } from "twin.macro";
 
 import { useRouter } from "next/router";
+
+import { PulseLoader as Loader } from "react-spinners";
 
 import Info from "../info/Info";
 import Button from "../buttons/Button";
@@ -15,16 +17,20 @@ interface PropsI {
 const OutOfStock: FC<PropsI> = ({ countInStock }) => {
   const { back } = useRouter();
 
+  const [load, setLoad] = useState<boolean>(false);
+
   return (
     <Fragment>
       {countInStock === 0 && (
         <section css={[tw`px-2`]}>
           <Info variant="blue" boldText="Out Of Stock">
             We will replenis this product very soon.{" "}
-            <span tw="inline-block">In the maean time you can: </span>
+            <span tw="inline-block">In the mean time you can: </span>
             <span tw="inline-block ml-1 mt-4">
               <Button
+                disabled={load}
                 onClick={() => {
+                  setLoad(true);
                   back();
                 }}
                 variant="primary"
@@ -33,6 +39,11 @@ const OutOfStock: FC<PropsI> = ({ countInStock }) => {
                 check our other products
               </Button>
             </span>
+            {load && (
+              <span tw="inline-block ml-1">
+                <Loader size={6} color="blue" />
+              </span>
+            )}
           </Info>
         </section>
       )}
