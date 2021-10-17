84c8b61e3c522a94d7def678ff0ffc893e279700
RadeDev <rade.bajic.dev@gmail.com>
2021-10-17 19:45:39 +0200

    🦉

--- components/products/ProductView.tsx
+++ components/products/ProductView.tsx
@@ -92,85 +92,87 @@ const ProductView: FC<PropsI> = ({ product }) => {
             {description}
           </p>
           <hr tw="my-3 w-11/12 mx-auto" />
-          {productIsNotInTheCart && (
-            <Fragment>
-              {countInStock !== 0 ? (
-                <Fragment>
-                  <div tw="mt-2">
-                    <label
-                      tw="flex justify-center dark:text-gray-400 text-gray-700 text-sm"
-                      htmlFor="count"
-                    >
-                      <span>Count:</span>
-                    </label>
-                    <div tw="flex justify-center">
-                      <div tw="flex items-center mt-1">
-                        <button
-                          onMouseDown={() => {
-                            handleCountUp();
-                          }}
-                          tw="text-gray-500 focus:outline-none focus:text-gray-600"
-                        >
-                          <svg
-                            tw="h-5 w-5"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
+          <div tw="border h-32">
+            {productIsNotInTheCart && (
+              <Fragment>
+                {countInStock !== 0 ? (
+                  <Fragment>
+                    <div tw="mt-2">
+                      <label
+                        tw="flex justify-center dark:text-gray-400 text-gray-700 text-sm"
+                        htmlFor="count"
+                      >
+                        <span>Count:</span>
+                      </label>
+                      <div tw="flex justify-center">
+                        <div tw="flex items-center mt-1">
+                          <button
+                            onMouseDown={() => {
+                              handleCountUp();
+                            }}
+                            tw="text-gray-500 focus:outline-none focus:text-gray-600"
                           >
-                            <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                          </svg>
-                        </button>
-                        <span tw="w-5 text-align[center] dark:text-gray-50 text-gray-700 text-lg mx-2">
-                          {productCount}
-                        </span>
-                        <button
-                          onMouseDown={() => {
-                            handleCountDown();
-                          }}
-                          tw="text-gray-500 focus:outline-none focus:text-gray-600"
-                        >
-                          <svg
-                            tw="h-5 w-5"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
+                            <svg
+                              tw="h-5 w-5"
+                              fill="none"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                            </svg>
+                          </button>
+                          <span tw="w-5 text-align[center] dark:text-gray-50 text-gray-700 text-lg mx-2">
+                            {productCount}
+                          </span>
+                          <button
+                            onMouseDown={() => {
+                              handleCountDown();
+                            }}
+                            tw="text-gray-500 focus:outline-none focus:text-gray-600"
                           >
-                            <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                          </svg>
-                        </button>
+                            <svg
+                              tw="h-5 w-5"
+                              fill="none"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                            </svg>
+                          </button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div tw="flex items-center mt-6 justify-center sm:justify-center  md:justify-center">
-                    <button tw="mb-6 flex px-8 py-2 bg-indigo-600 text-white text-sm font-medium rounded hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">
-                      Add To Cart
-                      <span tw="ml-1">
-                        <svg
-                          tw="h-5 w-5"
-                          fill="none"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                        </svg>
-                      </span>
-                    </button>
-                  </div>
-                </Fragment>
-              ) : (
-                <OutOfStockInfo countInStock={0} />
-              )}
-            </Fragment>
-          )}
+                    <div tw="flex items-center mt-6 justify-center sm:justify-center  md:justify-center">
+                      <button tw="mb-6 flex px-8 py-2 bg-indigo-600 text-white text-sm font-medium rounded hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">
+                        Add To Cart
+                        <span tw="ml-1">
+                          <svg
+                            tw="h-5 w-5"
+                            fill="none"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                          </svg>
+                        </span>
+                      </button>
+                    </div>
+                  </Fragment>
+                ) : (
+                  <OutOfStockInfo countInStock={0} />
+                )}
+              </Fragment>
+            )}
+          </div>
         </div>
       </div>
       {outOfBoundsUp && (
