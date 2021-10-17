8648df71db5f6050c06d56272a2e24be0fe77243
RadeDev <rade.bajic.dev@gmail.com>
2021-10-17 16:28:21 +0200

    🦉

--- components/alerts/Alert.tsx
+++ components/alerts/Alert.tsx
@@ -2,10 +2,14 @@
 import type { FC } from "react";
 import tw, { css, styled, theme } from "twin.macro";
 
+interface PropsI {
+  variant: "info" | "warning" | "error" | "success";
+}
+
 const Alert: FC = () => {
   return (
     <div tw="w-full fixed bottom-0.5 left-0">
-      <div tw="border border-green-500 flex w-full max-w-sm mx-auto overflow-hidden bg-gray-200 rounded-lg shadow-md dark:bg-gray-600">
+      <div tw="border-green-500 dark:bg-gray-700 bg-gray-200 border flex w-full max-w-sm mx-auto overflow-hidden rounded-lg shadow-md">
         <div tw="bg-green-500 flex items-center justify-center w-12">
           <svg
             tw="w-6 h-6 text-white fill-current"
