edf42391ebe41baf321c8efe1baf4991f2a87364
RadeDev <rade.bajic.dev@gmail.com>
2021-10-19 15:45:23 +0200

    🦉

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,3 @@
 export const PRODUCTS_PER_PAGE = 16;
+
+export const NAV_HISTORY = "NAV_HISTORY";
--- lib/storage/auth_nav_history.ts
+++ lib/storage/auth_nav_history.ts
@@ -2,4 +2,18 @@
 // HE SHOULD BE REDIRECTED TO THE signin PAGE
 // BUT AFTER SIGNIN HE SHOULD BE REDIRECTED TO THE PAGE HE REQUESTED
 
-export default 66;
+import cook from "js-cookie";
+
+import { NAV_HISTORY } from "@/constants/index";
+
+export const getNavHistory = () => {
+  return cook.get(NAV_HISTORY);
+};
+
+export const setNavHistory = (path: string) => {
+  if (!path.startsWith("/")) {
+    throw new Error("Your path should start with `/`");
+  }
+
+  return cook.set(NAV_HISTORY, path);
+};
