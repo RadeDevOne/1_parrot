58b546e66c42f1625d901688e6ef5bf498707365
RadeDev <rade.bajic.dev@gmail.com>
2021-10-20 17:26:38 +0200

    🦉

---
+++ lib/intent_nav/index.ts
@@ -0,0 +1,30 @@
+import type { NextPageContext } from "next";
+
+// import {getSession} from 'next-auth/react'
+
+import anh from "../storage/auth_nav_history";
+
+/**
+ *
+ * @param absPath absolute path of the protected route user wanted
+ * to initialy navigate
+ */
+export const setUserIntentNav = (absPath: string) => {
+  //
+  return anh.setNavHistory(absPath);
+};
+
+// THIS IS GOING TO BE NO OP
+/**
+ *
+ * @decrription NO-OP because path we are passing into signIn function as a
+ * callbackUrl  is going to be the path we need instead of path that this method provides
+ */
+export const redirectToUserIntentNav = (ctx: NextPageContext) => {
+  //
+  const navHist = anh.getNavHistory();
+
+  if (navHist === undefined) return;
+
+  ctx.res?.writeHead(302, { Location: navHist });
+};
