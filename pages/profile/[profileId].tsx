f09ec892ee936c8374eb92126fed5099b66feb6a
RadeDev <rade.bajic.dev@gmail.com>
2021-10-22 01:30:37 +0200

    🦉

--- pages/profile/[profileId].tsx
+++ pages/profile/[profileId].tsx
@@ -2,11 +2,11 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import type { GetServerSideProps, NextPage as NP } from "next";
 
-import { getSession } from "next-auth/react";
+// import { getSession } from "next-auth/react";
 
-import cookie from "cookie";
+// import cookie from "cookie";
 
-// import { buildUnAuthHistoryPathCookie } from "@/lib/intent_nav";
+import { redirectToSigninIfNoAuth } from "@/lib/intent_nav";
 
 interface PropsI {
   placeholder: boolean;
@@ -20,55 +20,18 @@ export const getServerSideProps: GetServerSideProps<
   PropsI | { nothing: true },
   paramsType
 > = async (ctx) => {
-  //
-  const query = ctx.query;
-  const paramz = ctx.params;
-  const resolvedUrl = ctx.resolvedUrl;
-  const defaultLocale = ctx.defaultLocale;
-  const locale = ctx.locale;
-  const locales = ctx.locales;
+  // REDIRECTING AND SETTING THE COOKIE IF THERE IS NO
+  // AUTHENTICATED USER
+  const redirectOptions = await redirectToSigninIfNoAuth(ctx, "/signin");
 
-  console.log(
-    // JSON.stringify(
-    {
-      query,
-      params: paramz,
-      resolvedUrl,
-      defaultLocale,
-      locale,
-      locales,
-    },
-    null,
-    2
-    // )
-  );
-
-  // console.log(buildUnAuthHistoryPathCookie(ctx));
-
-  /* const session = await getSession({ req: ctx.req });
-
-  const headers = ctx.req.headers;
-
-  const cookies = ctx.req.cookies;
-
-  console.log({ session, headers, cookies });
-
-  console.log({ session, headers });
-
-  const parsedCart = JSON.parse(ctx.req.cookies.CART);
-
-  console.log({ parsedCart }); */
-
-  /*   if(!session){
-
-      return {props: {
-        nothing: true
-      }}
-    } */
-
-  const { params } = ctx;
-
-  params?.siteId; //
+  if (redirectOptions.status === "unauthenticated") {
+    return {
+      props: {
+        nothing: true,
+      },
+      redirect: redirectOptions.redirect,
+    };
+  }
 
   return {
     props: {
