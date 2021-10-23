06d05229798dd1c1f88483c1824008cbc94ff7ae
RadeDev <rade.bajic.dev@gmail.com>
2021-10-23 19:59:37 +0200

    🦉

--- components/profile/MobileProfileButton.tsx
+++ components/profile/MobileProfileButton.tsx
@@ -1,13 +1,18 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import type { FC } from "react";
+import { Fragment } from "react";
 import tw, { css, styled, theme } from "twin.macro";
 
 import Link from "next/link";
+
+import { useRouter } from "next/router";
 
 import useProfileMenuData from "@/hooks/useProfileMenuData";
 
 const MobileProfileButton: FC = () => {
   const profileMenuData = useProfileMenuData();
+
+  const { asPath } = useRouter();
 
   if (!profileMenuData) {
     return null;
@@ -16,36 +21,43 @@ const MobileProfileButton: FC = () => {
   const { id, name, email, image } = profileMenuData;
 
   return (
-    <section css={[tw`flex w-min mt-1 pb-1`, tw`hover:text-indigo-600`]}>
-      {/*  */}
-      <hr />
-      <Link href={`/profile/${id}`}>
-        <a tw="mt-1 overflow-ellipsis flex items-center p-3 -mt-2 text-sm text-gray-600 transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
-          <div tw="pl-4">
-            <img
-              tw="flex-shrink-0 object-cover mx-1 rounded-full w-9 h-9"
-              src={image}
-              alt={`${name} avatar`}
-            />
-          </div>
+    <Fragment>
+      {!asPath.includes("/profile") && (
+        <section
+          css={[
+            tw`flex width[fit-content] mt-1 pb-1`,
+            tw`hover:text-indigo-600`,
+          ]}
+        >
+          {/*  */}
+          <hr />
+          <Link href={`/profile/${id}`}>
+            <a tw="hover:text-gray-500 dark:hover:text-gray-600 mt-1 overflow-ellipsis flex items-center p-3 -mt-2 text-sm text-gray-700 transition-colors duration-200 transform dark:text-gray-300">
+              <div tw="pl-4">
+                <img
+                  tw="flex-shrink-0 object-cover ml-0 rounded-full w-9 h-9"
+                  src={image}
+                  alt={`${name} avatar`}
+                />
+              </div>
 
-          <div tw="flex align-items[baseline] mx-1 overflow-ellipsis">
-            <h1 tw="text-sm font-semibold text-gray-700 dark:text-gray-200">
-              {name}
-              {email && ":"}
-            </h1>
-            {email && (
-              <p tw="pl-2 mt-1 overflow-ellipsis text-sm text-gray-500 dark:text-gray-400">
-                {email}
+              <div tw="flex align-items[baseline] ml-0 overflow-ellipsis">
+                <h1 tw="text-sm font-semibold">
+                  {name}
+                  {email && ":"}
+                </h1>
+                {email && (
+                  <p tw="pl-1 mt-1 overflow-ellipsis text-sm">{email}</p>
+                )}
+              </div>
+              <p tw="ml-0 underline w-full select-none overflow-ellipsis">
+                view profile
               </p>
-            )}
-          </div>
-          <p tw="ml-1 underline w-full select-none overflow-ellipsis">
-            view profile
-          </p>
-        </a>
-      </Link>
-    </section>
+            </a>
+          </Link>
+        </section>
+      )}
+    </Fragment>
   );
 };
 
