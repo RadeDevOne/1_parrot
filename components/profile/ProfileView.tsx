b5a4a7fedf2481f6f817e76392585729831b10da
RadeDev <rade.bajic.dev@gmail.com>
2021-10-24 00:13:54 +0200

    🦉

---
+++ components/profile/ProfileView.tsx
@@ -0,0 +1,18 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import type { FC } from "react";
+import tw, { css, styled, theme } from "twin.macro";
+
+const ProfileView: FC = () => {
+  return (
+    <section css={[tw`bg-gray-200`, tw`hover:text-indigo-600`]}>
+      <div
+        css={css`
+          background-color: ${theme`colors.electric`};
+        `}
+      ></div>
+      {/*  */}
+    </section>
+  );
+};
+
+export default ProfileView;
