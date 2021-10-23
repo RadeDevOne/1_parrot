33c9120072fc7d5b757b1f5739e60b413232c44f
RadeDev <rade.bajic.dev@gmail.com>
2021-10-23 20:05:57 +0200

    🦉

--- components/profile/MobileProfileButton.tsx
+++ components/profile/MobileProfileButton.tsx
@@ -4,8 +4,11 @@ import { Fragment } from "react";
 import tw, { css, styled, theme } from "twin.macro";
 
 import Link from "next/link";
-
 import { useRouter } from "next/router";
+
+import { useActor } from "@xstate/react";
+
+// import { , EE } from "@/machines/";
 
 import useProfileMenuData from "@/hooks/useProfileMenuData";
 
@@ -13,6 +16,8 @@ const MobileProfileButton: FC = () => {
   const profileMenuData = useProfileMenuData();
 
   const { asPath } = useRouter();
+
+  // const [___, dispatch] = useActor();
 
   if (!profileMenuData) {
     return null;
@@ -32,7 +37,11 @@ const MobileProfileButton: FC = () => {
           {/*  */}
           <hr />
           <Link href={`/profile/${id}`}>
-            <a tw="hover:text-gray-500 dark:hover:text-gray-600 mt-1 overflow-ellipsis flex items-center p-3 -mt-2 text-sm text-gray-700 transition-colors duration-200 transform dark:text-gray-300">
+            {/* eslint-disable-next-line */}
+            <a
+              // onMouseDown={() => dipatch()}
+              tw="hover:text-gray-500 dark:hover:text-gray-600 mt-1 overflow-ellipsis flex items-center p-3 -mt-2 text-sm text-gray-700 transition-colors duration-200 transform dark:text-gray-300"
+            >
               <div tw="pl-4">
                 <img
                   tw="flex-shrink-0 object-cover ml-0 rounded-full w-9 h-9"
