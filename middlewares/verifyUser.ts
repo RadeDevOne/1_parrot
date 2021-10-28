628f800323a0b79d35e35e28b3a7e9d484f7097c
RadeDev <rade.bajic.dev@gmail.com>
2021-10-28 04:18:13 +0200

    🦉

--- middlewares/verifyUser.ts
+++ middlewares/verifyUser.ts
@@ -10,11 +10,43 @@ const verifyUser: Middleware<NextApiRequest, NextApiResponse> = async (
   res,
   next
 ) => {
-  const { productId } = req.query;
-  //
-  //
-  //
-  //
+  // CHECKING IF WE HAVE SESSION
+  const session = await getSession({ req });
+
+  if (!session) {
+    return res
+      .status(401)
+      .send("Unauthenticated! Users session doesn't exists");
+  }
+  // FETCHING THE USER FROM OUR DATABASE
+  const profile = await prisma.profile.findUnique({
+    where: {
+      id: session.profile?.id,
+    },
+    select: {
+      id: true,
+      userId: true,
+    },
+  });
+  // CHECKING THAT WE ACTUALLY HAVE A USER IN THE DATBASE
+  if (!profile) {
+    return res
+      .status(401)
+      .send("Unauthenticated! We don't have such a user in our database");
+  }
+  // WE CAN CHECK IF ID IS OK
+  if (profile.id !== session.profile?.id) {
+    return res
+      .status(401)
+      .send("Unauthenticated! something is wrong with profile id");
+  }
+  // LETS CHECK USER TOO
+  if (session.userId !== profile.userId) {
+    return res.status(401).send("Unauthenticated! User ids mismatch");
+  }
+
+  // WE CAN CALL next HERE SINCE EVERYTHING SEEMS OK IN HERE
+
   next();
 };
 
