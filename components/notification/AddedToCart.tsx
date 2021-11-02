e88610cd0b5b30739a62f4ec211a572bdb503cfe
RadeDev <rade.bajic.dev@gmail.com>
2021-11-02 16:36:15 +0100

    🦉

--- components/notification/AddedToCart.tsx
+++ components/notification/AddedToCart.tsx
@@ -2,9 +2,17 @@
 import type { FC } from "react";
 import tw, { css, styled, theme } from "twin.macro";
 
+import { useActor } from "@xstate/react";
+
+import { cartService } from "@/machines/cart_machine";
+
 import Alert from "../alerts/Alert";
 
 const AddedToCart: FC = () => {
+  const [{ value, context }] = useActor(cartService);
+
+  console.log({ value, context });
+
   return (
     //<section css={[tw`bg-gray-200`, tw`hover:text-indigo-600`]}>
     <Alert header="Hello" text="i am here" variant="success" visible />
