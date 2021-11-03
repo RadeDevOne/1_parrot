7f38bc8bef8012da9c1bd7d1c2f9b1c9a55a71b3
RadeDev <rade.bajic.dev@gmail.com>
2021-11-03 18:08:57 +0100

    🦉

--- components/6_profile_stats/Layout.tsx
+++ components/6_profile_stats/Layout.tsx
@@ -6,22 +6,23 @@ import type { PropsI } from "@/pages/profile/stats/[profileId]";
 
 import Lorem from "@/components/dev-helpers/Lorem";
 
+import Favorites from "@/components/favorites/Favorites";
+
 const Layout: FC<PropsI> = ({ favorites }) => {
-  console.log({ favorites });
+  // console.log({ favorites });
 
   return (
     <main css={[tw``, tw``]}>
       {/* eslint-disable-next-line */}
       <h1>🦉 Profile Stats</h1>
-      Helo world
+      Helo World
       <h2 style={{ color: "blanchedalmond" }} id="favorites">
         Favorites
       </h2>
-      <Lorem />
+      <Favorites favorites={favorites} />
       <h2 style={{ color: "blanchedalmond" }} id="purchases">
         Past Purchases
       </h2>
-      <Lorem />
     </main>
   );
 };
--- components/dev-helpers/Stringified.tsx
+++ components/dev-helpers/Stringified.tsx
@@ -5,7 +5,9 @@ import tw, { css, styled, theme } from "twin.macro";
 const Stringified: FC<{ data: any }> = ({ data }) => {
   return (
     <section
-      css={[tw`bg-gray-200 text-black` /* , tw`hover:text-indigo-600` */]}
+      css={[
+        tw`bg-gray-200 text-black overflow-hidden` /* , tw`hover:text-indigo-600` */,
+      ]}
     >
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </section>
