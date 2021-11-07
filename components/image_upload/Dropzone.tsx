1a9725666f0a1ecb73804a708e78a9b95f2addae
RadeDev <rade.bajic.dev@gmail.com>
2021-11-07 21:54:05 +0100

    🦉

---
+++ components/image_upload/Dropzone.tsx
@@ -0,0 +1,14 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import type { FC } from "react";
+import tw, { css, styled, theme } from "twin.macro";
+
+const TestComponent: FC = () => {
+  return (
+    <section css={[tw``]}>
+      <div></div>
+      {/*  */}
+    </section>
+  );
+};
+
+export default TestComponent;
