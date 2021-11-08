6c0dd1c614086b3a1d7343f89a2c0f253625eab7
RadeDev <rade.bajic.dev@gmail.com>
2021-11-08 19:25:29 +0100

    🦉

--- components/image_upload/Dropzone.tsx
+++ components/image_upload/Dropzone.tsx
@@ -3,6 +3,8 @@ import type { FC } from "react";
 import { useState, Fragment } from "react";
 import tw, { css, styled, theme } from "twin.macro";
 
+import { PacmanLoader as Loader } from "react-spinners";
+
 import { useActor } from "@xstate/react";
 import { dropboxToggService, EE } from "@/machines/dropbox_togg_machine";
 
@@ -24,84 +26,105 @@ const Dropzone: FC = () => {
       {visible && (
         <div tw="fixed z-10 left-0 top-0 w-full h-full flex bg-black bg-opacity-60">
           <div tw=" p-4 w-max bg-gray-400  m-auto rounded-lg">
-            <div
-              onDrop={(e) => {
-                e.preventDefault();
-                // console.log({ e });
+            {!file ? (
+              <div
+                onDrop={(e) => {
+                  e.preventDefault();
+                  // console.log({ e });
 
-                // console.log(e.target);
+                  // console.log(e.target);
 
-                // @ts-ignore
-                const file = e.dataTransfer.files[0] as File;
+                  // @ts-ignore
+                  const file = e.dataTransfer.files[0] as File;
 
-                // console.log({ one: file });
+                  // console.log({ one: file });
 
-                if (file) {
-                  setFile(file);
-                }
+                  if (file) {
+                    setFile(file);
+                  }
 
-                //
+                  //
 
-                // console.log(file);
-              }}
-              onDragOver={(e) => {
-                e.preventDefault();
-                // console.log({ e });
-              }}
-              tw="p-5 relative width[279px] md:width[496px] border-4 border-dotted border-gray-300 rounded-lg"
-              // style={{ minWidth: "278px", maxWidth: "60vw" }}
-            >
-              <svg
-                tw="text-indigo-500 w-24 mx-auto mb-4"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+                  // console.log(file);
+                }}
+                onDragOver={(e) => {
+                  e.preventDefault();
+                  // console.log({ e });
+                }}
+                tw="p-5 relative width[279px] md:width[496px] border-4 border-dotted border-gray-300 rounded-lg"
+                // style={{ minWidth: "278px", maxWidth: "60vw" }}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                />
-              </svg>
-              <div tw=" flex flex-col w-max mx-auto text-center">
-                <label>
-                  <input
-                    /* onSelect={(e) => {
+                <svg
+                  tw="text-indigo-500 w-24 mx-auto mb-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                  />
+                </svg>
+                <div tw=" flex flex-col w-max mx-auto text-center">
+                  <label>
+                    <input
+                      /* onSelect={(e) => {
                       console.log({ e });
                     }} */
-                    onChange={(e) => {
-                      // console.log({ e });
+                      onChange={(e) => {
+                        // console.log({ e });
 
-                      // console.log(e.target.files[0]);
-                      // @ts-ignore
-                      const file = e.target.files[0] as File;
+                        // console.log(e.target.files[0]);
+                        // @ts-ignore
+                        const file = e.target.files[0] as File;
 
-                      // console.log({ two: file });
+                        // console.log({ two: file });
 
-                      if (file) {
-                        setFile(file);
-                      }
-                    }}
-                    tw="text-sm cursor-pointer w-36 hidden"
-                    type="file"
-                    // multiple
-                  />
-                  <div tw=" bg-indigo-600 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-indigo-500">
-                    Select
-                  </div>
-                </label>
+                        if (file) {
+                          setFile(file);
+                        }
+                      }}
+                      tw="text-sm cursor-pointer w-36 hidden"
+                      type="file"
+                      // multiple
+                    />
+                    <div tw=" bg-indigo-600 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-indigo-500">
+                      Select
+                    </div>
+                  </label>
 
-                <div tw=" text-indigo-500 uppercase">or drop files here</div>
+                  <div tw=" text-indigo-500 uppercase">or drop files here</div>
+                </div>
+                <button
+                  onClick={() => dispatch({ type: EE.TOGGLE })}
+                  css={[
+                    tw`absolute -top-10 -right-10 bg-white p-4 cursor-pointer hover:bg-gray-100 py-2 text-gray-600 rounded-full`,
+                  ]}
+                >
+                  X
+                </button>
               </div>
-              <button
-                onClick={() => dispatch({ type: EE.TOGGLE })}
-                tw=" absolute -top-10 -right-10 bg-white p-4 cursor-pointer hover:bg-gray-100 py-2 text-gray-600 rounded-full"
+            ) : (
+              <div
+                css={[
+                  css`
+                    & > * {
+                      /* margin-top: -12px; */
+                      position: relative !important;
+                      top: -12px !important;
+                      left: -19px !important;
+                    }
+                  `,
+
+                  tw`flex h-52 justify-center align-items[center]  p-5 relative width[279px] md:width[496px] border-4 rounded-lg`,
+                ]}
               >
-                X
-              </button>
-            </div>
+                <Loader size={18} color="crimson" />
+              </div>
+            )}
           </div>
         </div>
       )}
