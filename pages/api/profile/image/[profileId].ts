76cf8d337d4c6145b20f2443699f9765d691122e
RadeDev <rade.bajic.dev@gmail.com>
2021-11-08 23:46:59 +0100

    🦉

--- pages/api/profile/image/[profileId].ts
+++ pages/api/profile/image/[profileId].ts
@@ -1,9 +1,13 @@
 import nc from "next-connect";
+import type { Middleware } from "next-connect";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 // import type { Product, Favorite } from "@prisma/client";
 
 import prisma from "@/lib/prisma";
+
+import multer from "multer";
+
 import type { Profile } from "@prisma/client";
 // import { getSession } from "next-auth/react";
 
@@ -25,6 +29,46 @@ export interface ResData {
 
 export type BodyDataTypeI = ProfileDataType;
 
+const ALLOWED_FORMATS = ["image/jpg", "image/jpeg", "image/png"];
+
+// IN MEMORY STORAGE FOR IMAGES
+const storage = multer.memoryStorage();
+
+//
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    if (ALLOWED_FORMATS.includes(file.mimetype)) {
+      // IF MIME TYPE IS OK
+      cb(null, true);
+    } else {
+      cb(new Error("Wrong mime type for the file!"));
+    }
+  },
+});
+
+const singleUpload = upload.single("image");
+
+//
+const singleUploadMiddleware: Middleware<NextApiRequest, NextApiResponse> = (
+  req,
+  res,
+  next
+) => {
+  // @ts-ignore
+  singleUpload(req, res, (err) => {
+    if (err) {
+      return res
+        .status(422)
+        .send({ message: "Failed to process the image file!" });
+    }
+
+    next();
+  });
+};
+
+//
+
 // --------
 /* const profileBodyValidation = nc<NextApiRequest, NextApiResponse>().put(
   "/api/profile/:profileId",
@@ -32,16 +76,26 @@ export type BodyDataTypeI = ProfileDataType;
 ); */
 // ------------------
 
+const imageFileValidation = nc<NextApiRequest, NextApiResponse>().post(
+  "/api/profile/image/:profileId"
+);
+
 // MIDDLEWARES
 handler.use(validateProfileId);
 handler.use(verifyUserMiddleware);
 
 // THIS MIDDLEWARE IS ONLY GOING TO WORK FOR THIS ROUTE
-handler.post(async (req, res) => {
+handler.use(imageFileValidation).post(async (req, res) => {
   // @ts-ignore
   // const profile = req.profile as ProfileInsert;
 
   // console.log({ profile });
+
+  // BECAUSE OF MIDDLEWARE WE SHOUD HAVE req.file
+
+  const file = req.file as File;
+
+  console.log({ file });
 
   const body = req.body;
 
