438750f4c1262e712f8bb7201bac3f09abbacb5c
RadeDev <rade.bajic.dev@gmail.com>
2021-11-10 03:36:25 +0100

    🦉

--- components/breadcrumbs/ShipPayOrdBreadcrumbs.tsx
+++ components/breadcrumbs/ShipPayOrdBreadcrumbs.tsx
@@ -108,25 +108,49 @@ const ShipmentPaymentOrderBreadcrumbs: FC = () => {
       <nav tw="text-black font-bold my-8" aria-label="Breadcrumb">
         <ol tw="list-none p-0 inline-flex">
           {links.map(({ Icon, name, path }, i) => {
+            let paint = false;
+
+            for (const item of paintIndexes) {
+              if (item === i) {
+                paint = true;
+              }
+            }
+
             return (
               <li key={`${name}-${i}`} tw="flex items-center">
+                {i !== 0 && (
+                  <svg
+                    tw="fill-current w-3 h-3 mx-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 320 512"
+                    // stroke="crimson"
+                    // fill="red"
+                  >
+                    <path
+                      css={[
+                        !paint
+                          ? tw``
+                          : css`
+                              fill: ${theme`colors.__success`};
+                            `,
+                      ]}
+                      d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+                    />
+                  </svg>
+                )}
                 <Link href={path}>
-                  <a tw="flex align-items[center]">
+                  <a
+                    css={[
+                      paint ? tw`text-__success` : tw``,
+                      tw`flex align-items[center]`,
+                    ]}
+                  >
                     <span tw="mr-0.5">{name}</span>
                     <span>
                       <Icon />
                     </span>
                   </a>
                 </Link>
-                {i !== links.length - 1 && (
-                  <svg
-                    tw="fill-current w-3 h-3 mx-3"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 320 512"
-                  >
-                    <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
-                  </svg>
-                )}
               </li>
             );
           })}
