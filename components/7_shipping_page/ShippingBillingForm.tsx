e5d46955b1be0d3c7ba579dc31889703f6808a43
RadeDev <rade.bajic.dev@gmail.com>
2021-11-11 22:55:58 +0100

    🦉

--- components/7_shipping_page/ShippingBillingForm.tsx
+++ components/7_shipping_page/ShippingBillingForm.tsx
@@ -1,5 +1,5 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
-import type { FC } from "react";
+import type { FC, ChangeEventHandler } from "react";
 import { useState, Fragment } from "react";
 import tw, { css, styled, theme } from "twin.macro";
 
@@ -7,10 +7,17 @@ import type { Profile } from "@prisma/client";
 
 import { useSession } from "next-auth/react";
 
+import countries from "../../countries_n_states/1_countries.json";
+import states from "../../countries_n_states/2_states.json";
+
 const ShippingBillingForm: FC<{ initialProfilleInfo: Profile }> = ({
   initialProfilleInfo,
 }) => {
   const { data, status } = useSession();
+  // console.log({ initialProfilleInfo });
+
+  const [bodyData, setBodyData] =
+    useState<typeof initialProfilleInfo>(initialProfilleInfo);
 
   if (!data) {
     return null;
@@ -19,25 +26,53 @@ const ShippingBillingForm: FC<{ initialProfilleInfo: Profile }> = ({
     return null;
   }
 
-  const { id, nick, role } = data.profile;
+  const {
+    nick: name,
+    email,
+    streetAddress,
+    city,
+    country,
+    postalCode,
+    regionOrState,
+  } = bodyData;
 
-  console.log({ initialProfilleInfo });
+  console.log({ bodyData });
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const {
+      target: { name: inputName, value: inputValue },
+    } = e;
+    // @ts-ignore
+    handleBodyDataChange(inputName, inputValue);
+  };
+
+  const handleSelectChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    const {
+      target: { name: selName, value: selValue },
+    } = e;
+    // @ts-ignore
+    handleBodyDataChange(selName, selValue);
+  };
 
   //
   return (
     <div tw="leading-loose flex justify-center">
       <form
-        css={[tw`dark:bg-gray-800 bg-l max-w-xl m-4 p-10 rounded shadow-xl`]}
+        css={[
+          css``,
+
+          tw`dark:bg-gray-800 bg-l max-w-xl m-4 p-10 rounded shadow-xl`,
+        ]}
       >
         <p tw="text-gray-800 font-medium">Customer information</p>
         <div tw="">
-          <label tw="block text-sm text-gray-600" htmlFor="cus_name">
+          <label tw="block text-sm" htmlFor="nick">
             Name
           </label>
           <input
-            tw="w-full px-5 py-2 text-gray-700 bg-gray-200 rounded"
-            id="cus_name"
-            name="cus_name"
+            tw="w-full px-5 py-2 rounded"
+            id="nick"
+            name="nick"
             type="text"
             required
             placeholder="Your Name"
@@ -45,13 +80,13 @@ const ShippingBillingForm: FC<{ initialProfilleInfo: Profile }> = ({
           />
         </div>
         <div tw="mt-2">
-          <label tw="block text-sm text-gray-600" htmlFor="cus_email">
+          <label tw="block text-sm" htmlFor="email">
             Email
           </label>
           <input
-            tw="w-full px-5  py-2 text-gray-700 bg-gray-200 rounded"
-            id="cus_email"
-            name="cus_email"
+            tw="w-full px-5  py-2 rounded"
+            id="email"
+            name="email"
             type="text"
             required
             placeholder="Your Email"
@@ -59,13 +94,13 @@ const ShippingBillingForm: FC<{ initialProfilleInfo: Profile }> = ({
           />
         </div>
         <div tw="mt-2">
-          <label tw=" block text-sm text-gray-600" htmlFor="cus_email">
+          <label tw=" block text-sm" htmlFor="streetAddress">
             Address
           </label>
           <input
-            tw="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-            id="cus_email"
-            name="cus_email"
+            tw="w-full px-2 py-2 rounded"
+            id="streetAddress"
+            name="streetAddress"
             type="text"
             required
             placeholder="Street"
@@ -73,60 +108,74 @@ const ShippingBillingForm: FC<{ initialProfilleInfo: Profile }> = ({
           />
         </div>
         <div tw="mt-2">
-          <label tw="hidden text-sm block text-gray-600" htmlFor="cus_email">
+          <label tw="hidden text-sm block" htmlFor="city">
             City
           </label>
           <input
-            tw="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-            id="cus_email"
-            name="cus_email"
+            tw="w-full px-2 py-2 rounded"
+            id="city"
+            name="city"
             type="text"
             required
             placeholder="City"
-            aria-label="Email"
+            aria-label="City"
           />
         </div>
         <div tw="inline-block mt-2 w-1/2 pr-1">
-          <label tw="hidden block text-sm text-gray-600" htmlFor="cus_email">
+          <label tw="hidden block text-sm" htmlFor="country">
             Country
           </label>
-          <input
-            tw="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-            id="cus_email"
-            name="cus_email"
-            type="text"
+          <select
+            tw="w-full px-2 py-3 rounded"
+            id="country"
+            name="country"
             required
             placeholder="Country"
-            aria-label="Email"
-          />
+            aria-label="Country"
+          >
+            {countries.map((item, i) => {
+              return (
+                <option key={`${i}-${item.iso2}`} value={item.iso2}>
+                  {item.name}
+                </option>
+              );
+            })}
+          </select>
         </div>
         <div tw="inline-block mt-2 -mx-1 pl-1 w-1/2">
-          <label tw="hidden block text-sm text-gray-600" htmlFor="cus_email">
+          <label tw="hidden block text-sm" htmlFor="postalCode">
             Zip
           </label>
           <input
-            tw="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-            id="cus_email"
-            name="cus_email"
+            tw="w-full px-2 py-2 rounded"
+            id="postalCode"
+            name="postalCode"
             type="text"
             required
             placeholder="Zip"
-            aria-label="Email"
+            aria-label="Zip"
           />
         </div>
         <div tw="mt-2">
-          <label tw="hidden block text-sm text-gray-600" htmlFor="cus_state">
+          <label tw="hidden block text-sm" htmlFor="state">
             State
           </label>
-          <input
-            tw="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-            id="stat"
-            name="cus_state"
-            type="text"
+          <select
+            tw="w-full px-2 py-3 rounded"
+            id="state"
+            name="state"
             required
             placeholder="State"
             aria-label="State"
-          />
+          >
+            {states.map((item, i) => {
+              return (
+                <option key={`${i + item.state_code}`} value={item.state_code}>
+                  {item.name}
+                </option>
+              );
+            })}
+          </select>
         </div>
         {/* <p tw="mt-4 text-gray-800 font-medium">Payment information</p> */}
         {/* <div tw="">
@@ -145,7 +194,7 @@ const ShippingBillingForm: FC<{ initialProfilleInfo: Profile }> = ({
         </div> */}
         <div tw="mt-4">
           <button
-            tw="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded"
+            tw="mt-8 px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded"
             type="submit"
           >
             Save
