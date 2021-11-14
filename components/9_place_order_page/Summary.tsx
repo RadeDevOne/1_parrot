05ce8f4c76202666ec2b1236a780c72128c7fdc7
RadeDev <rade.bajic.dev@gmail.com>
2021-11-14 17:02:25 +0100

    🦉

---
+++ components/9_place_order_page/Summary.tsx
@@ -0,0 +1,16 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import type { FC } from "react";
+import tw, { css, styled, theme } from "twin.macro";
+
+const Summary: FC = () => {
+  return (
+    <section css={[tw``]}>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </section>
+  );
+};
+
+export default Summary;
