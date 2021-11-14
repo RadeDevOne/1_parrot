74199f4b46b2c19baabdeda41fc8bb44e5e0c8d8
RadeDev <rade.bajic.dev@gmail.com>
2021-11-15 00:00:36 +0100

    🦉

--- components/9_place_order_page/Summary.tsx
+++ components/9_place_order_page/Summary.tsx
@@ -4,7 +4,12 @@ import tw, { css, styled, theme } from "twin.macro";
 
 import type { ExpectedDataProps as PropsI } from "@/pages/place-order/[orderId]";
 
-import calculateOrderPrice from "@/lib/order/calculateOrderPrice";
+import {
+  calculatePriceWithoutShipping,
+  calculateTotalPrice,
+  getItemWithQuantityAndPrice,
+} from "@/lib/order/calculateOrderPrice";
+import formatPrice from "@/util/formatPrice";
 
 import countries from "../../countries_n_states/3_countries_by_key.json";
 import states from "../../countries_n_states/3_countries_by_key.json";
@@ -17,7 +22,9 @@ const Summary: FC<PropsI> = ({ order }) => {
 
   //
   //
-  const totalPrice = calculateOrderPrice(order);
+  const shippingPrice = order.shippingPrice;
+  const totalPrice = calculateTotalPrice(order);
+  const priceWithoutShipping = calculatePriceWithoutShipping(order);
 
   const buyerKeys = [
     "nick",
@@ -47,68 +54,62 @@ const Summary: FC<PropsI> = ({ order }) => {
             Order Summary
           </h1>
           <ul tw="py-6 border-b space-y-6 px-8">
-            <li tw="grid grid-cols-6 gap-2 border-b-2">
-              <div tw="col-span-1 self-center">
-                <img
-                  src="https://bit.ly/3oW8yej"
-                  alt="Product"
-                  tw="rounded w-full"
-                />
-              </div>
-              <div tw="flex flex-col col-span-3 pt-2">
-                <span tw="text-gray-600 text-lg font-semibold">
-                  Studio 2 Headphone
-                </span>
-                <span tw="text-gray-400 text-sm inline-block pt-2">
+            {order.items.map((item, i) => {
+              return (
+                <li
+                  key={i + item.id}
+                  tw="relative -left-3.5   grid grid-cols-6 gap-2 border-b-2"
+                >
+                  <div tw="col-span-1 self-center">
+                    <img
+                      src={item.product.image}
+                      alt={"Product: " + item.product.name}
+                      tw="rounded w-full"
+                    />
+                  </div>
+                  <div tw="flex flex-col col-span-3 pt-2">
+                    <span tw="text-gray-600 text-sm font-semibold">
+                      {item.product.name}
+                    </span>
+                    {/* <span tw="text-gray-400 text-sm inline-block pt-2">
                   Red Headphone
-                </span>
-              </div>
-              <div tw="col-span-2 pt-3">
-                <div tw="flex items-center space-x-2 text-sm justify-between">
-                  <span tw="text-gray-400">2 x €30.99</span>
-                  <span tw="text-pink-400 font-semibold inline-block">
-                    €61.98
-                  </span>
-                </div>
-              </div>
-            </li>
-            <li tw="grid grid-cols-6 gap-2 border-b-2">
-              <div tw="col-span-1 self-center">
-                <img
-                  src="https://bit.ly/3lCyoSx"
-                  alt="Product"
-                  tw="rounded w-full"
-                />
-              </div>
-              <div tw="flex flex-col col-span-3 pt-2">
-                <span tw="text-gray-600 text-lg font-semibold">
-                  Apple iPhone 13
-                </span>
-                <span tw="text-gray-400 text-sm inline-block pt-2">Phone</span>
-              </div>
-              <div tw="col-span-2 pt-3">
-                <div tw="flex items-center space-x-2 text-sm justify-between">
-                  <span tw="text-gray-400">1 x €785</span>
-                  <span tw="text-pink-400 font-semibold inline-block">
-                    €785
-                  </span>
-                </div>
-              </div>
-            </li>
+                </span> */}
+                  </div>
+                  <div tw="col-span-2 pt-3">
+                    <div tw="flex items-center space-x-2 text-sm justify-between">
+                      <span tw="text-gray-400">
+                        {item.quantity} x{" "}
+                        {formatPrice(item.product.price, "EUR")}
+                      </span>
+                      <span tw="text-pink-400 font-semibold inline-block">
+                        {formatPrice(
+                          getItemWithQuantityAndPrice(item).amount,
+                          "EUR"
+                        )}
+                      </span>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
           <div tw="px-8 border-b">
             <div tw="flex justify-between py-4 text-gray-600">
               <span>Subtotal</span>
-              <span tw="font-semibold text-pink-500">€846.98</span>
+              <span tw="font-semibold text-pink-500">
+                {formatPrice(priceWithoutShipping, "EUR")}
+              </span>
             </div>
             <div tw="flex justify-between py-4 text-gray-600">
               <span>Shipping</span>
-              <span tw="font-semibold text-pink-500">Free</span>
+              <span tw="font-semibold text-pink-500">
+                {formatPrice(shippingPrice, "EUR")}
+              </span>
             </div>
           </div>
           <div tw="font-semibold text-xl px-8 flex justify-between py-8 text-gray-600">
             <span>Total</span>
-            <span>€846.98</span>
+            <span>{formatPrice(totalPrice, "EUR")}</span>
           </div>
         </div>
         <div tw="pb-8 lg:col-span-2 col-span-3 space-y-8 lg:px-12 overflow-x-hidden">
@@ -149,24 +150,20 @@ const Summary: FC<PropsI> = ({ order }) => {
           <div tw="rounded-md lg:mx-2 mx-4">
             <section tw="p-6 rounded-md dark:bg-gray-700 bg-__secondary_dark">
               <h2 tw="uppercase tracking-wide text-lg font-semibold text-gray-700 my-2">
-                Payment Information
+                Payment Method
               </h2>
               <fieldset tw=" dark:bg-gray-700 bg-__secondary_dark  mb-3 shadow-lg rounded text-gray-600">
                 <label tw="flex border-gray-200 h-12 py-3 items-center">
-                  <span tw="text-right px-2">Card</span>
-                  <input
-                    name="card"
-                    tw="focus:outline-none px-3 w-full"
-                    placeholder="Card number MM/YY CVC"
-                    required={true}
-                  />
+                  <span tw="text-right px-2">{order.paymentMethod}</span>
                 </label>
               </fieldset>
             </section>
           </div>
-          <button tw="px-4 width[220px] mx-auto py-3 rounded-full bg-pink-400 text-white focus:ring focus:outline-none text-xl font-semibold transition-colors">
-            Pay €846.98
-          </button>
+          <div tw="flex justify-center">
+            <button tw="px-4 width[220px] mx-auto py-3 rounded-full bg-pink-400 text-white focus:ring focus:outline-none text-xl font-semibold transition-colors">
+              Place An Order
+            </button>
+          </div>
         </div>
       </div>
     </section>
