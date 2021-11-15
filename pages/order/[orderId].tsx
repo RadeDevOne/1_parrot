54dac3ec9325169761594116a7dab7c266347918
RadeDev <rade.bajic.dev@gmail.com>
2021-11-15 17:15:38 +0100

    🦉

--- pages/order/[orderId].tsx
+++ pages/order/[orderId].tsx
@@ -2,6 +2,11 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import type { GetServerSideProps, NextPage as NP } from "next";
 
+import { redirectToSigninIfNoAuth } from "@/lib/intent_nav";
+
+// TODO (USE THIS)
+import validateOrder from "@/lib/auth/validateOrder";
+
 interface PropsI {
   placeholder: boolean;
   orderId?: string;
@@ -11,30 +16,78 @@ type paramsType = {
   orderId: string;
 };
 
-export const getServerSideProps: GetServerSideProps<PropsI, paramsType> =
-  async (ctx) => {
-    const { params } = ctx;
+export const getServerSideProps: GetServerSideProps<
+  PropsI | { nothing: true },
+  paramsType
+> = async (ctx) => {
+  const { params } = ctx;
 
-    params?.orderId; //
+  params?.orderId; //
 
-    // TODO
-    // CHECKING AUTHENTICATION/AUTHORIZATION
-    // REDIRECTING IF SOMETHING IS WRONG
-    // FETCHING ORDER
-    // MAKING CALCULATIONS
-    // PASSING TOTAL PRICE
-    // AND SUBTOTAL PRICE
-    // AND SHIPPING PRICE, AND MAYBE SOME OTHER THINGS
-    // NAME OF THE USER
-    // AND MAYBE ALSO A ID OF AN ORDER
+  // TODO
+  // CHECKING AUTHENTICATION/AUTHORIZATION
+  // REDIRECTING IF SOMETHING IS WRONG
+  // FETCHING ORDER
+  // CHECKING STATUS OF AN ORDER
+  // MAKING CALCULATIONS
+  // PASSING TOTAL PRICE
+  // AND SUBTOTAL PRICE
+  // AND SHIPPING PRICE, AND MAYBE SOME OTHER THINGS
+  // NAME OF THE USER
+  // AND MAYBE ALSO A ID OF AN ORDER
 
+  const redirectOptions = await redirectToSigninIfNoAuth(ctx, "/signin");
+
+  if (redirectOptions.status === "unauthenticated") {
     return {
       props: {
-        placeholder: true,
-        orderId: params?.orderId,
+        nothing: true,
+      },
+      redirect: redirectOptions.redirect,
+    };
+  }
+
+  // WE WILL CHECK IF ORDER EXISTS
+  // IF NOT WE ARE GOING TO REDIRRECT TO THE MAIN PAGE
+
+  // WE WILL CHECK IF ORDER EXISTS
+  // IF NOT WE ARE GOING TO REDIRRECT TO THE MAIN PAGE
+  const order = await validateOrder(ctx);
+  if (order === "unauthorized" || order === "unauthenticated") {
+    return {
+      props: {
+        nothing: true,
+      },
+      redirect: {
+        destination: "/signin",
+        permanent: false,
       },
     };
+  }
+
+  if (
+    order.status === "PENDING" ||
+    order.status === "AWAITING_PAYMENT" ||
+    order.status === "AWAITING_ORDER_PLACEMENT"
+  ) {
+    return {
+      props: {
+        nothing: true,
+      },
+      redirect: {
+        destination: `/shipping/${order.id}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      placeholder: true,
+      orderId: params?.orderId,
+    },
   };
+};
 
 const Page: NP<PropsI> = (props) => {
   //
