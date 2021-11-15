e468b17c32e5625132ea006b42c8848ed054fb12
RadeDev <rade.bajic.dev@gmail.com>
2021-11-16 00:47:35 +0100

    🦉

---
+++ pages/api/config/paypal.ts
@@ -0,0 +1,21 @@
+import nc from "next-connect";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+
+const handler = nc<NextApiRequest, NextApiResponse>();
+
+handler.get(async (req, res) => {
+  const session = await getSession({ req });
+
+  // console.log({ session });
+
+  if (!session) {
+    return res
+      .status(401)
+      .send("Not authenticated!, Acessing to Pypal Client id not allowed!");
+  }
+
+  return res.status(200).send(process.env.PAYPAL_CLIENT_ID);
+});
+
+export default handler;
