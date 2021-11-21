7c4438ba08133979513a10a11c6f277233f199bb
RadeDev <rade.bajic.dev@gmail.com>
2021-11-21 14:27:46 +0100

    🦉

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,5 +1,6 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import type { FC } from "react";
+import { Fragment } from "react";
 import tw, { css, styled, theme } from "twin.macro";
 
 import { Splide, SplideSlide } from "@splidejs/react-splide";
@@ -8,29 +9,56 @@ import "@splidejs/splide/dist/css/themes/splide-skyblue.min.css";
 
 const Carousel: FC = () => {
   return (
-    <section>
-      <Splide
-        options={{
-          type: "loop",
-          gap: "1rem",
-          autoplay: true,
-          pauseOnHover: false,
-          resetProgress: false,
-          arrows: "slider",
-          height: "12rem",
-        }}
-        hasSliderWrapper
-        // hasAutoplayControls
-        hasAutoplayProgress
-      >
-        <SplideSlide>
-          <img src="/images/placeholder.jpg" alt="product" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="/images/placeholder.jpg" alt="product" />
-        </SplideSlide>
-      </Splide>
-    </section>
+    <Fragment>
+      <section tw="display[none] md:display[block]">
+        Desktop
+        <Splide
+          options={{
+            type: "loop",
+            gap: "1rem",
+            autoplay: true,
+            pauseOnHover: false,
+            resetProgress: false,
+            arrows: "slider",
+            height: "26rem",
+          }}
+          hasSliderWrapper
+          // hasAutoplayControls
+          hasAutoplayProgress
+        >
+          <SplideSlide>
+            <img src="/images/placeholder.jpg" alt="product" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src="/images/placeholder.jpg" alt="product" />
+          </SplideSlide>
+        </Splide>
+      </section>
+      <section tw="display[block] md:display[none]">
+        Mobile
+        <Splide
+          options={{
+            type: "loop",
+            gap: "1rem",
+            autoplay: true,
+            pauseOnHover: false,
+            resetProgress: false,
+            arrows: "slider",
+            height: "14rem",
+          }}
+          hasSliderWrapper
+          // hasAutoplayControls
+          hasAutoplayProgress
+        >
+          <SplideSlide>
+            <img src="/images/placeholder.jpg" alt="product" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src="/images/placeholder.jpg" alt="product" />
+          </SplideSlide>
+        </Splide>
+      </section>
+    </Fragment>
   );
 };
 
