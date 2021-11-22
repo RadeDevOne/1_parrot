bfcdbe09ea32d858e0a343af2b1b2f21fbe39dd8
RadeDev <rade.bajic.dev@gmail.com>
2021-11-22 17:09:22 +0100

    🦉

--- prisma/migrations/20211114133807_p_method_order/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `paymentMethod` column on the `Order` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Order" DROP COLUMN "paymentMethod",
-ADD COLUMN     "paymentMethod" "PaymentProvider";
